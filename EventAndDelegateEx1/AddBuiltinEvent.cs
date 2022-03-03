6c3cbb21fc3b14a27506aab201de77f5b4170237
jay.patel <jay.patel@1R-80>
2022-03-03 15:47:11 +0530

    Add project files.

---
+++ EventAndDelegateEx1/AddBuiltinEvent.cs
@@ -0,0 +1,25 @@
+﻿//Pass Data From one Class To another  using Event Delegate
+
+using System;
+
+namespace EventAndDelegateEx1
+{
+    public class AddBuiltinEvent
+    {
+        public event EventHandler<EventArgs> Addnumbers;
+        public void Add()
+        {
+            int x;
+            x = 100 + 150;
+            Console.WriteLine("\n Event Add " + (x));
+            Console.Read();
+            OnCompletedEvent();
+
+        }
+        private void OnCompletedEvent()
+        {
+            Addnumbers?.Invoke(this, EventArgs.Empty);
+            Console.Read();
+        }
+    }
+}
---
+++ EventAndDelegateEx1/Program.cs
@@ -0,0 +1,86 @@
+﻿//Pass Data From one Class To another  using Event Delegate
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAndDelegateEx1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StartProgram sp = new StartProgram();
+            sp.completeProcess += Sp_completeProcess;
+            sp.StartProcess();
+
+            EventDelegate ed = new EventDelegate();
+            ed.Plus += Ed_Plus;
+            ed.process();
+
+            AddBuiltinEvent abe = new AddBuiltinEvent();
+            abe.Addnumbers += Abe_Addnumbers;
+            abe.Add();
+        }
+
+        private static int Ed_Plus(int x, int y)
+        {
+            Console.WriteLine("\n x + y = " + (x + y));
+            return x + y;
+        }
+
+        private static void Sp_completeProcess()
+        {
+            Console.WriteLine("\n Welocme to the World of friends");
+        }
+
+        private static void Abe_Addnumbers(object sender, EventArgs e)
+        {
+            Console.WriteLine("\n Event is Executed.....");
+        }
+        
+       
+    }
+    
+
+}
+
+public class StartProgram
+{
+    public  delegate void start();
+
+    public event start completeProcess;
+
+    public void StartProcess()
+    {
+        Console.WriteLine("\n Process started");
+        Console.ReadLine();
+        OnCompletedProcess();
+    }
+    private void OnCompletedProcess()
+    {
+        completeProcess?.Invoke();
+    }
+}
+
+public class EventDelegate
+{
+    public delegate int Sum(int x, int y);
+
+    public event Sum Plus;
+    public void process()
+    {
+        Console.WriteLine("\n Process Started");
+        Console.ReadLine();
+        OnSumProcess();
+    }
+    private void OnSumProcess()
+    {
+        Plus.Invoke(30, 40);
+        Console.Read();
+    }
+}
+
+
